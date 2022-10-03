4ebc8b0a465908d8b2f3890af0875afda3aee927
Kevin Ryan <kevinryan1973@gmail.com>
2022-10-03 06:31:14 +0000

    add cusom acion for container packaging

---
+++ .github/actions/package-containers-github/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -l
+
+# Group --------------------------------------------------------------------------------------------
+echo "::group::Debug Support"
+    pwd
+    ls -a
+echo "::endgroup::"
+# End Group ----------------------------------------------------------------------------------------
+
+# Group --------------------------------------------------------------------------------------------
+echo "::group::Login To Container Registry"
+    docker login $INPUT_REGISTRY -u $INPUT_USER_NAME -p $INPUT_PASSWORD
+echo "::endgroup::"
+# End Group ----------------------------------------------------------------------------------------
+
+
+# Group --------------------------------------------------------------------------------------------
+echo "::group::Build Container Images"
+    # fibonacci-service
+    docker build -f Dockerfile -t ghcr.io/cprime-labs/cqa-demo:$INPUT_VERSION .
+echo "::endgroup::"
+# End Group ----------------------------------------------------------------------------------------
+
+# Group --------------------------------------------------------------------------------------------
+echo "::group::Push Container Images"
+    # fibonacci-service
+    docker push ghcr.io/cprime-labs/cqa-demo:$INPUT_VERSION
+echo "::endgroup::"
+# End Group ----------------------------------------------------------------------------------------
